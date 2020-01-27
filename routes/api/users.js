c749926d13249144bd2386ab93435027d9c0c76f
shaktichetan23 <hosdurga.s@husky.neu.edu>
2020-01-27 16:27:59 -0500

    Adding Assignment 2

---
+++ routes/api/users.js
@@ -0,0 +1,247 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const connection = require('../../config')
+var auth = require('basic-auth');
+const uuidv4 = require('uuid/v4');
+var passwordValidator = require('password-validator');
+var validator = require("email-validator");
+
+validatePass=(pass) => {
+  var schema = new passwordValidator();
+  schema
+  .is().min(8)
+  .has().uppercase()
+  .has().lowercase()
+  .has().digits()
+  return schema.validate(pass)
+}
+
+
+
+// @route   POST api/users
+// @desc    Register new user
+// @access  Public
+router.post('/', (req, res) => {
+  const { first_name,last_name, email_address, password } = req.body;
+  // console.log(req.body)
+  var check = false;
+
+  // Simple validationr
+  if(!first_name ||!last_name || !email_address || !password) {
+    return res.status(400).json({ msg: 'Please enter all fields' });
+  }
+  //Validate Email
+  if(!validator.validate(email_address)){
+    return res.status(400).json({msg:"Enter correct email address"})
+  }
+
+  //Check if email already exists
+  connection.query(`SELECT * FROM finaltable WHERE email_address = ? `,[email_address],
+  (err,row)=>{
+    if (err) {
+      console.log(err)
+    }
+
+  //Validate passwords
+  if(!validatePass(password)){
+    return res.status(400).json({msg: 'Password should minimus of 8 characters and be a combination of uppercase, lowercase, and digits'})
+
+  }
+
+    if(row.length>=1){
+      return res.status(400).json({
+        msg: "user exists"
+      });
+    }
+    else{
+      var newUser = {
+        first_name,
+        last_name,
+        email_address,
+        password
+      }
+      
+      var date=new Date() //Creating date for created_time
+
+      // Create salt & hash
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if(err) throw err;
+          newUser.password = hash;
+
+          var id = uuidv4(); //Generating uuid
+          connection.query(
+            `insert into finaltable(id,first_name, last_name, email_address, password, account_created, account_updated) 
+                      values(?,?,?,?,?,?,?)`,
+            [ id,
+              newUser.first_name,
+              newUser.last_name,
+            
+              newUser.email_address,
+              newUser.password,
+              date,
+              date
+            ],
+            (error, results, fields) => {
+              if (error) {
+                console.log(error);
+                res.status(500).json({
+                  msg:"Databse error"
+                })
+              }
+              else{
+                const {password,... data} = req.body 
+                
+                 console.log(results)
+                return res.status(201).json({
+                  ...data,
+                  account_created: date,
+                  account_updated: date
+                })
+              }
+            })
+          
+            
+        })
+      })
+
+    }
+  });
+});
+
+
+
+// @route   POST api/users
+// @desc    Get User
+// @access  Private
+router.get('/self' , (req,res) => {
+
+  var date = new Date();
+  var user = auth(req);
+  if(user==undefined||user==null){
+    return res.status(403).json({msg:"Unauthorized Access"})
+  
+  }
+  connection.query(`SELECT * FROM finaltable  WHERE email_address = ?`,[user.name],
+  (err,row,fields) => {
+    if(err) {
+      res.status(500).json({
+        msg:"Database Error"
+      });
+  
+    }
+    if(row.length==0){
+      res.status(400).json({
+        msg:"User doesnt exist"
+      });
+
+    }
+    
+    else{
+      bcrypt.compare(user.pass,row[0].password)
+      .then(isMatch => {
+        if(!isMatch) return res.status(400).json({ msg: 'Invalid credentials' });
+
+        else{
+          const {password, ...pl} = row[0];
+          res.status(200).json(pl)
+        }
+    });
+  }
+  });
+
+
+});
+
+// @route   PUT api/users/self
+// @desc    Update
+// @access  Private
+
+router.put("/self",(req,res) => {
+  const { first_name,last_name, email_address, password } = req.body;
+
+  //Simple Validations
+  if(!first_name ||!last_name || !email_address || !password) {
+    return res.status(400).json({ msg: 'Please enter all fields' });
+  }
+
+  //Basic Auth
+  var user = auth(req);
+  //Check for auth
+  if(user == null || user == undefined)
+    return res.status(403).json({msg:"Unauthorized Access"})
+
+  // Check for email
+  connection.query(`SELECT * FROM finaltable  WHERE email_address = ?`,[user.name],
+  (err,row,fields) => {
+    if(err) {
+      return res.status(500).json({
+        succ:"0",
+        msg:"Database Error"
+      });
+  
+    }
+
+
+    //Validate Email Adreess
+    if(!validator.validate(email_address)){
+      return res.status(400).json({msg:"Enter correct email address"})
+    }
+    connection.query(`SELECT * FROM finaltable  WHERE email_address = ?`,[email_address],
+    (err,row,fields) => {
+      if(err) throw err
+      if(row.length>=1 && email_address!=user.name){
+        return res.status(400).json({
+          msg:"User Already Exists"
+        })
+      }
+      return
+
+
+    });
+    if(row.length==0){
+      res.status(400).json({
+        msg:"User doesnt exist"
+      });
+
+    }
+    else{
+      bcrypt.compare(user.pass,row[0].password)
+      .then(isMatch => {
+        if(!isMatch) return res.status(401).json({ msg: 'Invalid Credentials' });
+
+        else{
+          bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(password, salt, (err, hash) => {
+              if(err) throw err;
+              var new_password = hash
+              var ud= new Date()
+              const queryStr = `UPDATE finaltable SET first_name = ?, last_name = ?, email_address = ?, password = ?, account_updated = ? WHERE email_address = ?`
+              connection.query(queryStr,[first_name,last_name,email_address,new_password, ud, user.name],
+                (err,row,fields) => {
+                  if(err){
+                    console.error(err)
+                    res.status(500).json({
+                      // succ: "-1",
+                      msg: "Database Error"
+                    });
+                  }
+                  // console.log(row)
+                  res.status(204)
+                  res.end()
+            }); 
+
+          });
+        });
+
+        
+          }
+
+      });
+    }
+  });
+});
+
+
+module.exports = router;
