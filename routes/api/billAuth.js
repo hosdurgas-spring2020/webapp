deee7f075f2042fccc80763372597bf70871cae8
shaktichetan23 <hosdurga.s@husky.neu.edu>
2020-01-30 11:27:47 -0500

    Adding config

--- routes/api/billAuth.js
+++ routes/api/billAuth.js
@@ -8,7 +8,7 @@ module.exports = billAuth = (req,res,next) => {
     ,(err,row) => {
         if(err) res.status("500").json({msg:"Database Error"})
         // console.log(row[0].owner_id)
-        if(row.length == 0) return res.status(400).json({msg:"No such bills"})
+        if(row.length == 0) return res.status(404).json({msg:"No such bills"})
         if(ownerid == row[0].owner_id){
             console.log("HERE")
             return next()
