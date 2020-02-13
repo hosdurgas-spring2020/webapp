cd0b844678affea07e96b3d276c2c72658bf86d1
shaktichetan23 <hosdurga.s@husky.neu.edu>
2020-02-12 19:29:33 -0500

    Added more functionality

--- routes/api/billAuth.js
+++ routes/api/billAuth.js
@@ -4,18 +4,19 @@ module.exports = billAuth = (req, res, next) => {
   const ownerid = res.locals.ownerid;
   // console.log(ownerid)
   connection.query(
-    `select owner_id from bill_table where id =?`,
+    `select owner_id, attachments from bill_table where id =?`,
     [req.params.id],
     (err, row) => {
       if (err) {
         console.error(err);
         return res.status("500").json({ msg: "Database Error" });
       }
-      // console.log(row[0].owner_id)
+      // console.log(row);
       if (row.length == 0)
         return res.status(404).json({ msg: "No such bills" });
       if (ownerid == row[0].owner_id) {
         // console.log("HERE")
+        res.locals.attachments = row[0].attachments;
         return next();
       }
 
