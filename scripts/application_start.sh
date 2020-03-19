8a9faa92ce9067f58798c98e9ffe0f5118b0bfed
shaktichetan23 <shaktichetan23>
2020-03-19 17:31:42 -0400

    Changes

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,6 +3,7 @@
 
 # Stop all servers and start the server as a daemon
 cd /webapp
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/webapp/test/api/statsConfig.json -s
 pm2 stop all
 sudo chown -R ubuntu .
 killall -9 node
